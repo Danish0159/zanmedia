84ac5aa840e2b6a699d0b782d9133deda5e2311d
Danish <danish01132@gmail.com>
2021-05-10 11:50:00 -0700

    portfolio added

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector(".portfolio__modal");
+const previews = document.querySelectorAll(".portfolio__grid img");
+const original = document.querySelector(".portfolio__full-img");
+previews.forEach((preview) => {
+    preview.addEventListener("click", () => {
+        modal.classList.add("open");
+        original.classList.add("open");
+        const originalSrc = preview.getAttribute("data-original");
+        original.src = `/img/${originalSrc}`
+    });
+});
+
+modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains("portfolio__modal")) {
+        modal.classList.remove("open");
+        original.classList.remove("open");
+    }
+});
