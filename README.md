3f1beb38e18c8a8e2cbc59660981d802b11ef885
Danish <danish01132@gmail.com>
2021-10-23 15:18:29 -0700

    Upwork Section added

--- README.md
+++
@@ -1,3 +0,0 @@
-# zanmedia
-This is a 1 page landing page that i built for a starting Marketing Agency who drives traffic to businesses by designing the instagram templates. Made using HTML, SASS, JavaScript etc.
-
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Modal
 const modal = document.querySelector(".portfolio__modal");
 const previews = document.querySelectorAll(".portfolio__grid img");
 const original = document.querySelector(".portfolio__full-img");
@@ -17,3 +18,18 @@ modal.addEventListener("click", (e) => {
     }
 });
 
+// Accordian
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
