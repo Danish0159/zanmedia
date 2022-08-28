eec176a380f1915aaab438f7c8ee2c020223e4f4
Danish <danish01132@gmail.com>
2022-08-28 16:16:38 +0500

    Update Report

--- js/pricing.js
+++ js/pricing.js
@@ -1,23 +1,39 @@
 const price = document.getElementById("price");
+// Default Value.
+price.value= 129;
 price.disabled = true;
-const allCheckBoxes = document.querySelectorAll('.input-checkbox');
+const allRadioBoxes = document.querySelectorAll(".input-radio");
 
 function chkcontrol() {
-    var sum = 0;
-    for (var i = 0; i < allCheckBoxes.length; i++) {
-        if (allCheckBoxes[i].checked) {
-            sum = sum + parseInt(allCheckBoxes[i].value);
-        }
+    if (allRadioBoxes[0].checked){
+        price.value = 129;
+    } 
+    else if (allRadioBoxes[1].checked) {
+      price.value = 179;
     }
-    price.value = sum;
 }
 
-let userChoice = document.querySelector('.userChoice');
-function YesOrNo() {
-    if (document.content.interested[0].checked) {
-        userChoice.style.display = 'block';
-    }
-    else if (document.content.interested[1].checked) {
-        userChoice.style.display = 'none';
-    }
-}
+
+// Old Code With checkboxes + Radio Buttons.
+// const price = document.getElementById("price");
+// price.disabled = true;
+// const allCheckBoxes = document.querySelectorAll(".input-checkbox");
+
+// function chkcontrol() {
+//   var sum = 0;
+//   for (var i = 0; i < allCheckBoxes.length; i++) {
+//     if (allCheckBoxes[i].checked) {
+//       sum = sum + parseInt(allCheckBoxes[i].value);
+//     }
+//   }
+//   price.value = sum;
+// }
+
+// let userChoice = document.querySelector(".userChoice");
+// function YesOrNo() {
+//   if (document.content.interested[0].checked) {
+//     userChoice.style.display = "block";
+//   } else if (document.content.interested[1].checked) {
+//     userChoice.style.display = "none";
+//   }
+// }
